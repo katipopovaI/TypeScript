388eafe21304c5be9cd4f810d2f32a5d03911c5d
«katipopovaI» <katipopova@mail.ru>
2023-06-08 15:21:29 +0300

    type TS

---
+++ type/types.js
@@ -0,0 +1,36 @@
+// const str: string = "hello";
+// console.log(str);
+//boolean type
+var isFetching = true;
+var isLoading = false;
+//number
+var int = 42;
+//float
+var float = 4.2;
+//string
+var message = "Hello";
+//array
+var numberArray = [1, 1, 2, 3, 5, 8, 13];
+var numberArray2 = [1, 1, 2, 3, 5, 8, 13];
+var words = ["hello", "TS"];
+//tuple
+var contact = ["ved", 1234];
+//any
+var variable = 42;
+//....
+variable = "new String";
+//function
+//void-ничего не вернет
+function sayMyName(name) {
+    console.log(name);
+}
+sayMyName("hi");
+//never
+//error
+//постоянно делает что-либо
+function throwError(message) {
+    throw new Error(message);
+}
+var login = "admin";
+var id1 = 1234;
+var id2 = "a12";
